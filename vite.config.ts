3afc8873ca07b6718ad177b631c7825e410e24b2
tojacob03 <to_jacob@me.com>
2026-09-19 18:03:47 +0200

    Register feedsPlugin (generates sitemap.xml + rss.xml on build)

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { feedsPlugin } from "./vite-plugins/feeds";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -11,6 +12,7 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
+    feedsPlugin(),
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
