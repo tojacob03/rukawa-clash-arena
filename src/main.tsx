6939069ca544d15ecff065e9008aeea93d7374ad
tojacob03 <to_jacob@me.com>
2026-09-20 16:04:51 +0200

    Install the silent dev console easter egg on app start

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { installDevConsole } from './lib/devConsole'
+
+installDevConsole();
 
 createRoot(document.getElementById("root")!).render(<App />);
