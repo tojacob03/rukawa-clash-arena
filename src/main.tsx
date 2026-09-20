904119f7f869885aa262386f0a3ec5bd0e5706d7
tojacob03 <to_jacob@me.com>
2026-09-21 00:27:46 +0200

    Remove browser-console easter egg install - replaced by an in-page terminal panel in the footer

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { installDevConsole } from './lib/devConsole'
-
-installDevConsole();
 
 createRoot(document.getElementById("root")!).render(<App />);
